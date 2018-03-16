540c0e6de58e44d99e9d18639ff013b19a02cadf
Jesus Francisco Dicent Aguero <jesusdicent@gmail.com>
2018-03-16 01:12:30 -0400

    Finishing Today

--- ZzaDashboard/ZzaDesktop/Customers/AddEditCustomerView.xaml.cs
+++ ZzaDashboard/ZzaDesktop/Customers/AddEditCustomerView.xaml.cs
@@ -24,5 +24,8 @@ namespace ZzaDesktop.Customers
         {
             InitializeComponent();
         }
+
+
+
     }
 }
--- ZzaDashboard/ZzaDesktop/Customers/AddEditCustomerViewModel.cs
+++ ZzaDashboard/ZzaDesktop/Customers/AddEditCustomerViewModel.cs
@@ -11,12 +11,36 @@ namespace ZzaDesktop.Customers
     {
         private bool _editMode;
         private Customer _editingCustomer = null;
+
         public bool EditMode { get => _editMode;
             set => SetProperty(ref _editMode, value); }
+
+        private SimpleEditableCustomer _customer =null;
+
+        public SimpleEditableCustomer Customer
+        {
+            get { return _customer; }
+            set { SetProperty(ref _customer , value); }
+        }
+
 
         public void SetCustomer(Customer obj)
         {
             _editingCustomer = obj;
+            Customer = new SimpleEditableCustomer();
+            CopyCustomer(obj, Customer);
+        }
+
+        private void CopyCustomer(Customer source, SimpleEditableCustomer target)
+        {
+            target.Id = source.Id;
+            if (EditMode)
+            {
+                target.FirstName = source.FirstName;
+                target.LastName = source.LastName;
+                target.Phone = source.Phone;
+                target.Email = source.Email;
+            }
         }
     }
 }
